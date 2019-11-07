ece25586a985857e9f48980e488972ec22e01c04
Rifian Joe Premilenio <55788355+RifianJoe@users.noreply.github.com>
2019-11-07 21:35:14 +0800

    Add files via upload

---
+++ IF1-10118013-Latihan47-NilaiMahasiswa/src/if1/pkg10118013/latihan47/nilaimahasiswa/IF110118013Latihan47NilaiMahasiswa.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118013.latihan47.nilaimahasiswa;
+
+/**
+ *
+ * @author Rifian
+ * Nama : Rifian Joe Premilenio
+ * Kelas : IF-01
+ * NIM : 10118013
+ */
+public class IF110118013Latihan47NilaiMahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NilaiMahasiswa mhs = new NilaiMahasiswa(75,45,34);
+        NilaiMahasiswa mhs1 = new NilaiMahasiswa(60, 80, 75);
+        NilaiMahasiswa mhs2 = new NilaiMahasiswa(30, 80, 40);
+        
+    }
+    
+}
